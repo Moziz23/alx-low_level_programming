d94cf1a885738ace7cbbf413f2b2c1d9cee47a45
Moziz23 <lucky.emeron7@gmail.com>
2023-08-11 12:35:01 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
-int main() 
+int main(void) 
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
-    return 1;
+	const char message[] = ("and that piece of art is useful\n", "Dora Korpar, 2015-10-19",);
+    syscall("SYS_write, STDERR_FILENO, message", sizeof(message) - 1);
+    return (1);
 }
