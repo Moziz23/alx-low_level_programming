bb29e4ae3b66f64fdc1d83451b125dcfe33c6081
Moziz23 <lucky.emeron7@gmail.com>
2023-08-11 06:06:23 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Return:always 0 (Success)
+ */
+int main(void)
+{
+	printf("\"With proper grammar, but the outcome is a piece of art,\n -Wall gcc");
+	return (0);
+}
