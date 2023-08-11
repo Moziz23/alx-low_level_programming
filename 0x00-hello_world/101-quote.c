86dbefea53f5137fa6cea0beecb3be026169318a
Moziz23 <lucky.emeron7@gmail.com>
2023-08-11 12:52:58 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-       	const char message[] =("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    syscall("SYS_write, STDERR_FILENO, message", sizeof((message) - 1);
-    return (1)
+	const char message[] = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
+    return (1);
 }
