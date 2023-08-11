a4471dc526dd26530c6e8ecc12de04d5c33562a1
Moziz23 <lucky.emeron7@gmail.com>
2023-08-11 06:13:51 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	printf("\"With proper grammar, but the outcome is a piece of art,\n -Wall gcc");
+	printf("\"With proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
