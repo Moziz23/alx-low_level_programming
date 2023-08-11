ae3b868776965f0fff57c611027c92ef63271bba
Moziz23 <lucky.emeron7@gmail.com>
2023-08-11 12:23:13 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+
+int main() 
+{
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
+    return 1;
+}
