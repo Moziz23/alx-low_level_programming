b1df19bd7f56abd0d856f34908f1eb94db04f579
Moziz23 <lucky.emeron7@gmail.com>
2023-08-12 16:54:20 -0700

    combin4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,19 +18,19 @@ int main(void)
 		{
 			for (int k = j + 1; k < 9; k++)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
 				if (i < 7)
 				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					continue;
+
 					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
 	}
-}
-putchar('\n');
-
-return (0);
+	putchar('\n');
+	return (0);
 }
