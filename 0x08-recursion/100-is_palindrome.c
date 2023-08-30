a5f90a1130d0ab1cb864117d008815d622edc6e8
Moziz23 <lucky.emeron7@gmail.com>
2023-08-30 09:18:36 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -35,6 +35,7 @@ int is_palindrome(char *s)
  * check - checker for the palindrome
  * @s: string
  * @start: int moves from right to left
+ * @end: int moves from left to right
  * @mod: int
  * Return: 0 or 1
  */
