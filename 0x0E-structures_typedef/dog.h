f43f1a607a732c344f8eeb4fdcb7c174cce5230d
Moziz23 <lucky.emeron7@gmail.com>
2023-09-11 07:40:18 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,14 +2,14 @@
 #define DOG_H
 
 /**
- * struct my_dog - a dog struct
+ * struct dog - a dog struct
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
  * Description: just a lone dog struct in a big world
  */
 
-struct my_dog
+struct dog
 {
 	char *name;
 	float age;
