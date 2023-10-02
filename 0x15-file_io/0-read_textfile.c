b32a757852a8794203c720e1ab7810e4089ef62e
Moziz23 <lucky.emeron7@gmail.com>
2023-10-02 11:06:35 -0700

    file io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_texfile - read texts from a file and prints it
+ * read_textfile - reads text from a file and prints it
  * @filename: name of file to read
  * @letters: number of bytes to read
  * Return: number bytes read/ printed
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
-	char buf[READ_BUF_SIZE *8];
+	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes = 0, len = _strlen(text_content);
-	
+
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
@@ -38,5 +38,5 @@ int create_file(const char *filename, char *text_content)
 	if (len)
 		bytes = write(fd, text_content, len);
 	close(fd);
-	return (bytes == len ? 1: -1);
+	return (bytes == len ? 1 : -1);
 }
